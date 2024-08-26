4fa95351359e9e0ece2218925f5d141c19c79b8e
dutysniper <natarov.constantin@yandex.ru>
2024-08-26 15:18:21 +0300

    Initial commit

---
+++ ToDoViperCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoViperCoreData
+//
+//  Created by Константин Натаров on 26.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
